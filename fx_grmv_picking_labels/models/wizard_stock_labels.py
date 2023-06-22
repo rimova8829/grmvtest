7eab32c6b7f176fc63a0b5a4be1ba6377c10d58f
Germán Ponce Dominguez <german.ponce@argil.mx>
2023-06-21 20:22:34 -0600

    Actualizaciones

--- fx_grmv_picking_labels/models/wizard_stock_labels.py
+++ fx_grmv_picking_labels/models/wizard_stock_labels.py
@@ -283,12 +283,31 @@ class WizardStowageLabels(models.TransientModel):
         list_products = []
         dict_products_qty = {}
         dict_products_lots = {}
+        dict_products_qty_by_lot = {}
+
         for line in picking_id.move_line_ids_without_package:
+            if line.lot_id:
+                if line.product_id.id not in dict_products_qty_by_lot:
+                    dict_products_qty_by_lot[line.product_id.id] = {'lot_ids':{
+                                                                                line.lot_id: line.qty_done
+                                                                              }}
+                else:
+                    prev_lot_ids = dict_products_qty_by_lot[line.product_id.id]['lot_ids']
+                    if line.lot_id in prev_lot_ids:
+                        prev_lot_qty = prev_lot_ids[line.lot_id]
+                        new_lot_qty = prev_lot_qty + line.qty_done
+                        dict_products_qty_by_lot[line.product_id.id]['lot_ids'][line.lot_id] = new_lot_qty
+                    else:
+                        dict_products_qty_by_lot[line.product_id.id]['lot_ids'][line.lot_id] = line.qty_done
+
+
             if line.product_id.id not in list_products:
                 list_products.append(line.product_id.id)
                 dict_products_qty[line.product_id.id] = line.qty_done
                 if line.lot_id:
                     dict_products_lots[line.product_id.id] = line.lot_id.name
+                if line.product_id.putaway_rule_ids:
+                    dict_products_putaway_rules[line.product_id.id] = line.product_id.putaway_rule_ids
             else:
                 line_pev_qty = dict_products_qty[line.product_id.id] 
                 line_new_qty = line_pev_qty + line.qty_done
@@ -301,61 +320,122 @@ class WizardStowageLabels(models.TransientModel):
         # lot_names = ','.join(
         #     picking_id.move_line_ids_without_package.mapped('lot_id.display_name')
         # )
-        
-        lot_names = ""
-        if list_products[0] in dict_products_lots:
-            lot_names = dict_products_lots[list_products[0]]
-        
-        qa_initials = picking_id.check_ids.mapped('user_id.name')
-        if not len(qa_initials):
-            qa_initials = ''
-        else:
-            qa_initials = map(lambda p: p[0], qa_initials[0].split(' '))
-            qa_initials = '.'.join(qa_initials)
-
-        mrp_prod_id = MrpProd.search([('name', 'ilike', f'{picking_id.origin}%%')])
-        if not len(mrp_prod_id):
-            raise UserError(f'No se encontró la orden de fabricación {picking_id.origin}')
-
-        date_finished = mrp_prod_id[0].date_finished
-        mo_date = date_finished.strftime('%d/%m/%Y') if date_finished else ""
-        
-        storage_location = picking_id.move_line_ids_without_package\
-            .mapped('product_id.putaway_rule_ids')
-        if not len(storage_location):
-            storage_location = picking_id.location_dest_id.display_name
-        else:
-            storage_location = storage_location[0].location_out_id.display_name
-        
-        product_br = self.env['product.product'].browse(list_products[0])
-        product_name = product_br.display_name
-        
-        total_qty = int(dict_products_qty[list_products[0]])
-        lines = []
-        list_records = []
-        sublist = []
-        count_l = 0
-        for idx in range(1, total_qty + 1):
-            xvals = {
-                        'pn' : product_name,
-                        'mo' : picking_id.origin,
-                        'lot' : lot_names,#'LOTE', 
-                        'qa' : qa_initials,#'INICIALES',
-                        'storage_location' : storage_location,#'UBICACION ALMACENAJE',
-                        'mo_date' : mo_date,#'FECHA FABRICACION',
-                        'count' : f'{idx} de {total_qty}'
-                    }
-            lines.append(xvals)
-            if count_l < 500:
-                sublist.append(xvals)
-                count_l += 1
+        if list_products[0] in dict_products_qty_by_lot:
+            product_br = self.env['product.product'].browse(list_products[0])
+            product_name = product_br.display_name
+            qa_initials = picking_id.check_ids.mapped('user_id.name')
+            if not len(qa_initials):
+                qa_initials = ''
             else:
-                list_records.append(sublist)
-                sublist = [xvals]
-                count_l = 1
+                qa_initials = map(lambda p: p[0], qa_initials[0].split(' '))
+                qa_initials = '.'.join(qa_initials)
 
-        if sublist and sublist not in list_records:
-            list_records.append(sublist)
+            mrp_prod_id = MrpProd.search([('name', 'ilike', f'{picking_id.origin}%%')])
+            if not len(mrp_prod_id):
+                raise UserError(f'No se encontró la orden de fabricación {picking_id.origin}')
+
+            date_finished = mrp_prod_id[0].date_finished
+            mo_date = date_finished.strftime('%d/%m/%Y') if date_finished else ""
+            
+            storage_location = "ALM/Existencias"
+
+            # if not len(storage_location):
+            #     storage_location = picking_id.location_dest_id.display_name
+            # else:
+            #     storage_location = storage_location[0].location_out_id.display_name
+            lines = []
+            list_records = []
+            sublist = []
+            count_l = 0
+
+            lots_dict = dict_products_qty_by_lot[list_products[0]]['lot_ids']
+            for lot in lots_dict.keys():
+
+                lot_names = lot.name
+
+                total_qty = int(lots_dict[lot])
+                # total_qty = int(dict_products_qty[list_products[0]])
+                
+                for idx in range(1, total_qty + 1):
+                    xvals = {
+                                'pn' : product_name,
+                                'mo' : picking_id.origin,
+                                'lot' : lot_names,#'LOTE', 
+                                'qa' : qa_initials,#'INICIALES',
+                                'storage_location' : storage_location,#'UBICACION ALMACENAJE',
+                                'mo_date' : mo_date,#'FECHA FABRICACION',
+                                'count' : f'{idx} de {total_qty}'
+                            }
+                    lines.append(xvals)
+                    if count_l < 500:
+                        sublist.append(xvals)
+                        count_l += 1
+                    else:
+                        list_records.append(sublist)
+                        sublist = [xvals]
+                        count_l = 1
+
+                if sublist and sublist not in list_records:
+                    list_records.append(sublist)
+
+        else:
+            lot_names = ""
+            if list_products[0] in dict_products_lots:
+                lot_names = dict_products_lots[list_products[0]]
+            
+            qa_initials = picking_id.check_ids.mapped('user_id.name')
+            if not len(qa_initials):
+                qa_initials = ''
+            else:
+                qa_initials = map(lambda p: p[0], qa_initials[0].split(' '))
+                qa_initials = '.'.join(qa_initials)
+
+            mrp_prod_id = MrpProd.search([('name', 'ilike', f'{picking_id.origin}%%')])
+            if not len(mrp_prod_id):
+                raise UserError(f'No se encontró la orden de fabricación {picking_id.origin}')
+
+            date_finished = mrp_prod_id[0].date_finished
+            mo_date = date_finished.strftime('%d/%m/%Y') if date_finished else ""
+            
+            # storage_location = picking_id.move_line_ids_without_package\
+            #     .mapped('product_id.putaway_rule_ids')
+
+            storage_location = "ALM/Existencias"
+
+            if not len(storage_location):
+                storage_location = picking_id.location_dest_id.display_name
+            else:
+                storage_location = storage_location[0].location_out_id.display_name
+            
+            product_br = self.env['product.product'].browse(list_products[0])
+            product_name = product_br.display_name
+            
+            total_qty = int(dict_products_qty[list_products[0]])
+            lines = []
+            list_records = []
+            sublist = []
+            count_l = 0
+            for idx in range(1, total_qty + 1):
+                xvals = {
+                            'pn' : product_name,
+                            'mo' : picking_id.origin,
+                            'lot' : lot_names,#'LOTE', 
+                            'qa' : qa_initials,#'INICIALES',
+                            'storage_location' : storage_location,#'UBICACION ALMACENAJE',
+                            'mo_date' : mo_date,#'FECHA FABRICACION',
+                            'count' : f'{idx} de {total_qty}'
+                        }
+                lines.append(xvals)
+                if count_l < 500:
+                    sublist.append(xvals)
+                    count_l += 1
+                else:
+                    list_records.append(sublist)
+                    sublist = [xvals]
+                    count_l = 1
+
+            if sublist and sublist not in list_records:
+                list_records.append(sublist)
 
         picking_id.single_labels_printed = True
         if self.split_labels:
