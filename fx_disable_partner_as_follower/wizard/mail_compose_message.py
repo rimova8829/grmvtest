a8daf0ccef921eb0350d970b8490f2df79e281dd
Germán Ponce Dominguez <german.ponce@argil.mx>
2023-06-22 23:25:46 -0600

    Actualizaciones

--- fx_disable_partner_as_follower/wizard/mail_compose_message.py
+++ fx_disable_partner_as_follower/wizard/mail_compose_message.py
@@ -24,3 +24,13 @@ from odoo import _, api, fields, models
 class MailComposer(models.TransientModel):
     _inherit = 'mail.compose.message'
     
+    def send_mail_action(self):
+        # TDE/ ???
+        ctx = dict(self._context) or {}
+        ctx.update({'block_follower_mail':True})
+        self.env['mail.compose.message'].with_context(ctx)
+        res = super(MailComposer, self.with_context(ctx)).send_mail_action()
+        return res
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
